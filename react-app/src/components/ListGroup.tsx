934d4bd54a4d7bcf5553fa61fa6fec30b25c97f4
Hatim Alouani <haalouan@e3r7p7.1337.ma>
2025-07-28 20:43:18 +0100

    push

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,29 @@
+// import { Fragment } from "react/jsx-runtime";
+
+import { Fragment } from "react/jsx-runtime";
+
+function ListGroup() {
+	let items = [
+		'New York',
+		'Tokyo',
+		'London',
+		'Casa Blanca'
+	];
+	return (
+	<>
+		<h1>List</h1>
+		{items.length === 0 && <p>No item found</p>}
+		<ul className="list-group">
+			{items.map((item) =>(
+				<li className="list-group-item"
+					key={item}
+					onClick={() => console.log(item )}
+				>
+					{item}
+				</li>))}
+		</ul>
+	</>
+  );
+}
+
+export default ListGroup;
